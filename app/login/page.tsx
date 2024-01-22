f4310ab629c169612a7844f0a2acb4d9625a464f
Brendan Chess <66980517+brendan-chess@users.noreply.github.com>
2024-01-21 21:51:01 -0600

    prisma setup
    created a mongodb database and setup prisma orm. currently has a basic user model and login returns if the entered username exists

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,31 +1,50 @@
+"use client"
+
 import Link from "next/link";
+import { useState } from "react";
+
+async function handleLogin(username: string) {
+  const res = await fetch('/api/login', {
+    method: 'POST',
+    body: JSON.stringify({ username })
+  })
+  const userExists = await res.json()
+  console.log(userExists)
+}
 
 export default function Page() {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+
+
   return (
     <div className="flex flex-col gap-4 items-center">
-      <div className="text-xl text-blue-900 my-4 underline underline-offset-8">
+      <div className="text-xl text-slate-200 my-4 underline underline-offset-8">
         Log in
       </div>
       <div className="flex flex-col gap-1">
-        <div className="text-blue-900 ml-3 font-semibold text-sm">Username</div>
+        <div className="text-slate-200 ml-3 font-semibold text-sm">Username</div>
         <input
           type="text"
-          className="border-2 border-blue-700 text-blue-900 rounded-lg shadow px-2 py-1"
+          onChange={(event) => setUsername(event.target.value)}
+          className="bg-slate-700 rounded-lg shadow px-2 py-1"
         />
       </div>
       <div className="flex flex-col gap-1">
-        <div className="text-blue-900 ml-3 font-semibold text-sm">Password</div>
+        <div className="text-slate-200 ml-3 font-semibold text-sm">Password</div>
         <input
           type="password"
-          className="border-2 border-blue-700 text-blue-900 rounded-lg shadow px-2 py-1"
+          onChange={(event) => setPassword(event.target.value)}
+          className="bg-slate-700 rounded-lg shadow px-2 py-1"
         />
       </div>
-      <Link
-        href="/"
-        className="bg-blue-700 hover:bg-blue-600 text-blue-50 px-4 py-2 w-fit rounded-lg font-bold shadow mt-4"
+      <button
+        className="bg-slate-700 text-slate-200 px-4 py-2 w-fit rounded-lg font-bold shadow mt-4"
+        onClick={() => handleLogin(username)}
       >
         Submit
-      </Link>
+      </button>
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,10 @@ import Link from "next/link";
 export default function Page() {
   return (
     <div className="flex flex-col">
-      <div className="text-xl text-blue-900">Hello</div>
+      <div className="text-xl text-slate-200">Hello</div>
       <Link
         href={"/login"}
-        className="bg-blue-700 hover:bg-blue-600 text-blue-50 px-4 py-2 w-fit rounded-lg font-bold shadow"
+        className="bg-slate-700 text-slate-200 px-4 py-2 w-fit rounded-lg font-bold shadow mt-4"
       >
         Log in
       </Link>
